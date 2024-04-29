5fb6e3e40a147676fadbaf0ea1829f73ac2fe3a2
shawn123115 <41225215+shawn123115@users.noreply.github.com>
2024-04-29 20:44:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["BSIT-1C STUDENTS", "MAJOR IN COMP-TECH","Our Desired Grade is 1.0"],
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+  });
